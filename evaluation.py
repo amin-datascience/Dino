a5cf1a2d56789ce85e45433c5904e2aa9b2a13ae
Amin Beigli <amin.beigli22@gmail.com>
2022-06-09 16:52:52 +0430

    syntax error

--- evaluation.py
+++ evaluation.py
@@ -3,7 +3,6 @@ import torch
 import torch.nn as nn 
 from sklearn.neighbors import KNeighborsClassifier 
 from sklearn.metrics import accuracy_score  
-from sklearn.neighbors import akbar
 from sklearn.linear_model import LogisticRegression
 
 
